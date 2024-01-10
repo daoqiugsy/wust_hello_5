d30992f480b6a9fff119b3b61e416caa7f772849
1242435445@qq.com <1242435445@qq.com>
2024-01-10 21:12:25 +0800

    first commit

---
+++ src/test/java/com/wust_hello/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wust_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
