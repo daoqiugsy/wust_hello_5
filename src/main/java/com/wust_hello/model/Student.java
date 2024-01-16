334cc64785d46eaf9b2bc20a936d76d866aaea5f
1242435445@qq.com <78654431+daoqiugsy@users.noreply.github.com>
2024-01-16 23:01:21 +0800

    增加学生实体类

---
+++ src/main/java/com/wust_hello/model/Student.java
@@ -0,0 +1,23 @@
+package com.wust_hello.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Student {
+        private Integer id;
+        private String name;
+        private Integer collegeId;//学院外键
+        private Integer majorId;
+        private Integer grade;
+        private String edu;
+        private LocalDateTime createTime;
+        private LocalDateTime updateTime;
+        private int delete;//是否删除
+    }
+
