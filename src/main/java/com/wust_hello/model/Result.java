45f4d408c5f80fb40d42b8be1c65af314152289a
1242435445@qq.com <78654431+daoqiugsy@users.noreply.github.com>
2024-01-16 13:03:34 +0800

    增加返回结果Result类

---
+++ src/main/java/com/wust_hello/model/Result.java
@@ -0,0 +1,19 @@
+package com.wust_hello.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Result {
+    private Integer code;//0失败，1成功
+    private String message;//响应信息
+    private Object data;//响应数据
+    //失败响应
+    //增删改成功响应
+    public static Result success(){return new Result(1,"success",null);}
+    public static Result success(Object data){return new Result(1,"success",data);}
+    public static Result error(String message){return new Result(0,message,null);}
+}
