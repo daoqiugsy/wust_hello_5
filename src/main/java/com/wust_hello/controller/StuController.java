108b9268224ff90827b01fe7215db5f6388a83f8
1242435445@qq.com <78654431+daoqiugsy@users.noreply.github.com>
2024-01-16 23:02:18 +0800

    增加stuController类

---
+++ src/main/java/com/wust_hello/controller/StuController.java
@@ -0,0 +1,32 @@
+package com.wust_hello.controller;
+
+import com.wust_hello.model.PageBean;
+import com.wust_hello.model.Result;
+import com.wust_hello.model.Student;
+import com.wust_hello.service.StuService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+//学生管理
+@Slf4j
+@RestController
+
+public class StuController {
+    //private static Logger log = Logger.getLogger(StuController.class);
+    //查询学生信息
+    @Autowired
+    private StuService stuService;
+    @GetMapping("teacher/search_id")
+    public Result page(@RequestParam(defaultValue ="1") Integer page,
+                       @RequestParam(defaultValue ="10") Integer pageSize,String name) {
+        log.info("分页查询，参数：{}，{},{}", page,pageSize,name);
+        PageBean pagebean =stuService.page(page, pageSize,name);
+        return Result.success(pagebean);
+    }
+
+
+}
