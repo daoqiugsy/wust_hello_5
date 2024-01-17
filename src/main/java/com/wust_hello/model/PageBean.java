b1cfbc1e8264c9dae6c65856fca1ca39a870ca8e
1242435445@qq.com <78654431+daoqiugsy@users.noreply.github.com>
2024-01-17 13:22:48 +0800

    增加分页类

---
+++ src/main/java/com/wust_hello/model/PageBean.java
@@ -0,0 +1,14 @@
+package com.wust_hello.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageBean {
+    private Long total;//总记录数
+    private List stus;//数据列表
+}
