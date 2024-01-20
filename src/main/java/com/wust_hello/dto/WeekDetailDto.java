ec3461f29a88d1f8b59e3aab94b8483e76903952
daoqiugsy <1242435445@qq.com>
2024-01-20 21:34:42 +0800

    add

--- src/main/java/com/wust_hello/dto/WeekDetailDto.java
+++ src/main/java/com/wust_hello/dto/WeekDetailDto.java
@@ -1,16 +1,21 @@
 package com.wust_hello.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WeekDetailDto {
     Integer id;
-    LocalDate startTime;
-    LocalDate endTime;
-    String currentPlan;
-    String nextPlan;
-    Boolean isPush;
+    String name;
+    LocalDate start_time;
+    LocalDate end_time;
+    String current_plan;
+    String next_plan;
+    Boolean is_push;
     String sentiment;
 }
