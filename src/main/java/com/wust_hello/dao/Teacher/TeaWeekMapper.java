2eb56288093c984e9a0fc283d58f8f79dd7fa01e
daoqiugsy <1242435445@qq.com>
2024-01-22 16:35:45 +0800

    增加token

--- src/main/java/com/wust_hello/dao/Teacher/TeaWeekMapper.java
+++ src/main/java/com/wust_hello/dao/Teacher/TeaWeekMapper.java
@@ -2,6 +2,7 @@ package com.wust_hello.dao.Teacher;
 
 import com.wust_hello.dto.WeekDetailDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,5 +11,8 @@ public interface TeaWeekMapper {
     public List<WeekDetailDto> list(String name, LocalDate start_time, LocalDate end_time);
 
     public WeekDetailDto getById(Integer id);
+    @Select("select * from user where id=#{id}")
+    public  Boolean isgetid(Long id);
+
 
 }
