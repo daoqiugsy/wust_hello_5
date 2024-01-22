914ccfd72de1bea71dfe38fb35b41d5eeef227c1
daoqiugsy <1242435445@qq.com>
2024-01-22 16:32:07 +0800

    增加token

--- src/main/java/com/wust_hello/service/teacher/StuService.java
+++ src/main/java/com/wust_hello/service/teacher/StuService.java
@@ -2,7 +2,9 @@ package com.wust_hello.service.teacher;
 
 import com.wust_hello.common.Result;
 import com.wust_hello.model.PageBean;
+import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -12,5 +14,6 @@ public interface StuService {
 
    PageBean page(Integer page,Integer pageSize,String name,String token);
    //导出学生信息
-   public Result download(HttpServletResponse response) throws IOException;
+   public Result download(HttpServletResponse response, String token) throws IOException;
+
 }
