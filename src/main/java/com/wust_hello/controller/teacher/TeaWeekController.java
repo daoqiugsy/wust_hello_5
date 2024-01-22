2f1bda0bfecf7133e2cb12c2101af27b2845b81b
daoqiugsy <1242435445@qq.com>
2024-01-22 16:30:08 +0800

    增加token

--- src/main/java/com/wust_hello/controller/teacher/TeaWeekController.java
+++ src/main/java/com/wust_hello/controller/teacher/TeaWeekController.java
@@ -1,17 +1,15 @@
 package com.wust_hello.controller.teacher;
 
 import com.wust_hello.common.Result;
-import com.wust_hello.dto.StudentDto;
 import com.wust_hello.dto.WeekDetailDto;
 import com.wust_hello.model.PageBean;
-import com.wust_hello.model.Student;
 import com.wust_hello.service.teacher.WeekService;
-import com.wust_hello.service.teacher.StuService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 
 @Slf4j
@@ -27,16 +25,17 @@ public class TeaWeekController {
                        @RequestParam(defaultValue ="10") Integer pageSize,
                        String name,
                        @RequestParam(value="start_time",defaultValue = "")@DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate start_time,
-                       @RequestParam(value="end_time",defaultValue = "")@DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate end_time) {
+                       @RequestParam(value="end_time",defaultValue = "")@DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate end_time,
+                       HttpServletRequest request) {
         log.info("分页查询，参数：{}，{},{},{},{}", page,pageSize,name,start_time,end_time);
-        PageBean pagebean =weekService.page(page, pageSize,name,start_time,end_time);
+        PageBean pagebean =weekService.page(page, pageSize,name,start_time,end_time,request.getHeader("token"));
         return Result.success(pagebean);
     }
     //根据id查询
     @GetMapping("details/{id}")
-    public Result details(@PathVariable("id") Integer id){
+    public Result details(@PathVariable("id") Integer id, HttpServletRequest request){
         log.info("根据id查询，参数：{}", id);
-        WeekDetailDto weekDetailDto = weekService.getById(id);
+        WeekDetailDto weekDetailDto = weekService.getById(id,request.getHeader("token"));
         return Result.success(weekDetailDto);
     }
 }
