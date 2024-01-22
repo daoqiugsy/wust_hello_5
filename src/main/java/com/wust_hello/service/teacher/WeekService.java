485e95450a4e13150c49e4eee65bc21020192332
daoqiugsy <1242435445@qq.com>
2024-01-22 16:32:34 +0800

    增加token

--- src/main/java/com/wust_hello/service/teacher/WeekService.java
+++ src/main/java/com/wust_hello/service/teacher/WeekService.java
@@ -3,11 +3,13 @@ package com.wust_hello.service.teacher;
 import com.wust_hello.dto.WeekDetailDto;
 import com.wust_hello.model.PageBean;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 @Component
+
 public interface WeekService {
-    PageBean page(Integer page, Integer pageSize, String name, LocalDate start_time, LocalDate end_time);
+    PageBean page(Integer page, Integer pageSize, String name, LocalDate start_time, LocalDate end_time,String token);
     //根据ID查询
-    WeekDetailDto getById(Integer id);
+    WeekDetailDto getById(Integer id,String token);
 }
