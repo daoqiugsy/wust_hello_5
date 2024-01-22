145537ad032fd4fda2fd5c18e6b578175e33637d
daoqiugsy <1242435445@qq.com>
2024-01-22 16:35:12 +0800

    增加token

--- src/main/java/com/wust_hello/dao/Teacher/StuMapper.java
+++ src/main/java/com/wust_hello/dao/Teacher/StuMapper.java
@@ -2,13 +2,15 @@ package com.wust_hello.dao.Teacher;
 
 import com.wust_hello.dto.StudentDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
-
 @Mapper
 public interface StuMapper {
     public List<StudentDto> list(String name);
 
     public List<StudentDto> findAll();
+    @Select("select * from user where id=#{id}")
+    public Boolean isgetid(Long id);
 }
 
