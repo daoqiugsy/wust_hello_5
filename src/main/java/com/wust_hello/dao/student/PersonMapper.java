056dfa1d204308eec7fc06243843d679ac82c73b
TenshouinW <156179333+TenshouinW@users.noreply.github.com>
2024-01-26 20:58:19 +0800

    Create PersonMapper.java

---
+++ src/main/java/com/wust_hello/dao/student/PersonMapper.java
@@ -0,0 +1,27 @@
+package com.wust_hello.dao.student;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wust_hello.dto.CollegeDto;
+import com.wust_hello.dto.StudentDto;
+import com.wust_hello.model.College;
+import com.wust_hello.model.Major;
+import com.wust_hello.model.Student;
+import org.apache.ibatis.annotations.Param;
+
+
+import java.util.List;
+
+public interface PersonMapper extends BaseMapper<Student> {
+
+    Student selectById(Long id);
+
+    List<College> allCollege();
+
+    List<Major> selectByPageId(Long collegeId);
+
+    void updateById(@Param("student") Student student, @Param("stuId") Long stuId);
+
+    College selectByCollegeId(Long collegeId);
+
+    Major selectByMajorId(Long majorId);
+}
