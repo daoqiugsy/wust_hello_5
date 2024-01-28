9bbc7f44dbda2672255985bfd77087855c952609
imAnswer <109322271+imAnswer@users.noreply.github.com>
2024-01-28 15:31:46 +0800

    Delete helloproject directory

--- helloproject/src/main/java/com/wust_hello/mapper/LeisureMapper.java
+++
@@ -1,8 +0,0 @@
-package com.wust_hello.mapper;
-
-import org.apache.ibatis.annotations.Param;
-
-public interface LeisureMapper {
-    //空闲人数查询（/teacher/getfreenumber）
-    int selectLeisure(@Param("time_slot")int time_slot,@Param("date")String date);
-}
