12c89d4308831583a057eff01a8c6199922d025d
TenshouinW <156179333+TenshouinW@users.noreply.github.com>
2024-01-29 17:05:03 +0800

    Create StudentDto.java

---
+++ src/main/java/com/wust_hello/dto/student/StudentDto.java
@@ -0,0 +1,17 @@
+package com.wust_hello.dto.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StudentDto {
+    private Long id;
+    private String name;
+    private String college;//学院外键
+    private String major;//专业名称
+    private Integer grade;//年级名称
+    private String edu;
+}
