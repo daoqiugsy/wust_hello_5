3767beebd39475da1c7b1e6847cc9fbac7c8a5cc
TenshouinW <156179333+TenshouinW@users.noreply.github.com>
2024-01-29 21:55:59 +0800

    优化打卡记录查询

--- src/main/java/com/wust_hello/dao/student/CardMapper.java
+++ src/main/java/com/wust_hello/dao/student/CardMapper.java
@@ -1,17 +1,21 @@
 package com.wust_hello.dao.student;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wust_hello.dto.student.CardBueaty;
 import com.wust_hello.model.Card;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+
+
 @Mapper
 public interface CardMapper extends BaseMapper<Card> {
 
 
     void add(Card card);
 
-    List<Card> selectAllByStuId(Long userId);
+    List<CardBueaty> selectCardGroup(@Param("userId") Long userId);
 
-
+    List<Card> selectByDate(Long userId, String date);
 }
