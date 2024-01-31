2ddf52ae787b5b7b87f0e44a7c8c3454c769e08d
imAnswer <109322271+imAnswer@users.noreply.github.com>
2024-01-31 14:12:57 +0800

    Add files via upload

---
+++ src/main/java/com/wust_hello/utilpojo/LeisureMsg.java
@@ -0,0 +1,18 @@
+package com.wust_hello.utilpojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LeisureMsg {
+    int id;
+    String time;
+    int Mon;
+    int Tues;
+    int Wednes;
+    int Thurs;
+    int Fri;
+}
